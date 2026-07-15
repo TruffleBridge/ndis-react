c20373747af21c7d930b328a8d1ccede2dcc77d5
Raman <smartraman88@gmail.com>
2026-07-15 11:22:47 +0530

    base api url changes

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // base url from the env
 // const base_url = import.meta.env.VITE_API_URL;
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "http://52.66.162.16/api",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
